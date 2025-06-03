265aedda52ec8a1a8e57c0c16cf6784805c4d83c
Anton <a79153918440@yandex.ru>
2025-06-04 00:42:48 +0300

    sprint#6: add code style correction

--- src/ru/yandex/practicum/taskmanagerapp/history/HistoryManager.java
+++ src/ru/yandex/practicum/taskmanagerapp/history/HistoryManager.java
@@ -5,7 +5,9 @@ import ru.yandex.practicum.taskmanagerapp.task.Task;
 import java.util.List;
 
 public interface HistoryManager {
+
     void add(Task task);
+
     void remove(int id);
 
     List<Task> getHistory();
